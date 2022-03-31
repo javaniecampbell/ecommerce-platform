f2c3a19911156ad3926d1fc33f5baec548af8763
Javanie Campbell <1676496+javaniecampbell@users.noreply.github.com>
2022-03-31 18:37:11 +0000

    feat(infrastructure): added relationships and rename

--- .infrastructure/migrations/V1.5__Added_Category_Scehma_Table.sql
+++ .infrastructure/migrations/V1.5__Added_Category_Scehma_Table.sql
@@ -2,7 +2,7 @@ SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
-CREATE TABLE [dbo].[Category](
+CREATE TABLE [dbo].[Categories](
     [Id] [int] IDENTITY(1,1) NOT NULL,
     [Name] [varchar](50) NOT NULL,
     [Description] [varchar](max) NULL,
